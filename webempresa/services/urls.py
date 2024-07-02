ec4aeec873aced718508a797f1a7790b25ae9907
KdevG <toni.mrtnz.cn@gmail.com>
2024-07-02 12:05:38 +0200

    Reinicio completo

--- webempresa/services/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views 
-
-urlpatterns = [
-    path('servicios/', views.services , name = "services"),
-]
--- webempresa/services/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def services(request):
-    
-    return render(request , 'services/servicios.html')

f0f2b954c59fb6f0d0655e0a977f8f445677cddc
KdevG <toni.mrtnz.cn@gmail.com>
2024-07-02 13:39:29 +0200

    Reinicio

---
+++ web_empresa/core/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render , HttpResponse
+
+# Create your views here.
+
+
+def home(request):
+    
+    return render(request ,'core/base.html')
+
+
+def about(request):
+   
+    return render(request ,'core/about.html')
+
+
+def services(request):
+    
+    return render(request , 'core/services.html')
+
+
+def blog(request):
+    
+    return render(request , 'core/blog.html')
+
+
+def store(request):
+    
+    return render(request , 'core/store.html')
+
+
+def contact(request):
+    
+    return render(request , 'core/contact.html')
+
+
+def sample(request):
+    
+    return render(request , 'core/sample.html')
+
+
